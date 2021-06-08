4229f1d76798d99b64a0dd656bc6eb64f3610b79
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-06-08 18:07:18 -0500

    schema added

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE database tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL(15,2) NULL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name DECIMAL(15,2) NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+

dfbecc5ef7e87aa4d1dd3d6bf9e39ace30894f65
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-07-06 14:22:14 -0500

    finished seed data for schema and view functions are working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -22,7 +23,7 @@ const runInquirer = () => {
       message: 'What would you like to do?',
       choices: [
         'View All Employees',
-        "View All Departments"
+        "View All Employees By Role",
         'View All Employees By Department',
         'View All Managers',
         'View All Roles',
@@ -42,8 +43,8 @@ const runInquirer = () => {
           viewEmployees();
           break;
         
-        case 'View All Departments':
-          viewDepartments();
+        case 'View All Employees By Role':
+          viewEmployeesByRoles();
           break;
 
         case 'View All Employees By Department':
@@ -94,3 +95,31 @@ const runInquirer = () => {
       }
     });
 };
+
+function viewEmployees() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    runInquirer()
+  })
+}
+
+function viewEmployeesByRoles() {
+  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    runInquirer()
+  })
+}
+
+function viewEmployeesByDepartment() {
+  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    runInquirer()
+  })
+}
+
--- schema.sql
+++ schema.sql
@@ -4,25 +4,69 @@ CREATE database tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NULL
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL(15,2) NULL,
   department_id INT,
-  PRIMARY KEY (id)
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
-  PRIMARY KEY (id)
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
+
+-- SEEDS
+INSERT INTO department (name)
+VALUE ("Marketing");
+INSERT INTO department (name)
+VALUE ("Technology");
+INSERT INTO department (name)
+VALUE ("Accounting");
+INSERT INTO department (name)
+VALUE ("Administrative");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 180000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Administrative Team Lead", 190000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 80000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Marketing Lead", 90000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Marketing Coordinator", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Administraitve Assistant", 50000, 4);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jose", "Martinez", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Levi", "Cohen", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Alexis","Larsen",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jeffery", "Strickland", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Shelby", "Christianson", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Hubert", "Oswald", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Yael", "Chaim", 2, 7);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
